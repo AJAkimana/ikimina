f8f09c22abbf82d11ff2066288fc078af80e1092
“Kevine <ikevine2016@gmail.com>
2024-09-12 15:26:51 +0200

    implent ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import ThemeProvider from "@mui/material";
+import ThemeProvider, { Button } from "@mui/material";
 
 function App() {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Button>Click me</Button>
+    </ThemeProvider>
+  );
 }
 export default App;
