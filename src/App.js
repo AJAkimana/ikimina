4039fd88d6bba2a7175eb5721c00bfeedfe7ebbc
“Kevine <ikevine2016@gmail.com>
2024-09-12 15:05:37 +0200

    adding data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import ThemeProvider from "@mui/material";
 
 function App() {
-  return;
-  <ThemeProvider theme={theme}></ThemeProvider>;
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
 export default App;
