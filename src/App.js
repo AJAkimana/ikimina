1383d82687c0b38490e2cd451c6010d4f05e6db8
“Kevine <ikevine2016@gmail.com>
2024-09-12 16:23:30 +0200

    adding data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
 import { Button, ThemeProvider, createTheme } from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#1976d2", // Deep Sky Blue
-    },
-    secondary: {
-      main: "#663399", // Rich Plum/Lavender
-    },
-  },
-});
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
---
+++ src/common/theme.js
@@ -0,0 +1,10 @@
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2", // Deep Sky Blue
+    },
+    secondary: {
+      main: "#663399", // Rich Plum/Lavender
+    },
+  },
+});
