46390b3ef9f6f9e42759abf77cbb35b1cece32bd
“Kevine <ikevine2016@gmail.com>
2024-09-17 10:50:20 +0200

    importing the route file into App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button, ThemeProvider } from "@mui/material";
 import { theme } from "./common/theme";
+import RouteFiles from "./route.files";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Button>Click me</Button>
+     <RouteFiles/>
     </ThemeProvider>
   );
 }
