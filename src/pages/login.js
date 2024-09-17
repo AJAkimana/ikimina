037d2715c2d894ed2fbc375f1d23a1aa6b55fa46
“Kevine <ikevine2016@gmail.com>
2024-09-17 11:26:41 +0200

    adding the data

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <div>
       <h1>This is the Login page</h1>
+      <Link to="/">Click to view our Home page</Link>
     </div>
   );
 }
