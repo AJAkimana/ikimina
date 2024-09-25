e4441e84cf25639f1d9ae9d6a0a67ba2342d9d3a
“Kevine <ikevine2016@gmail.com>
2024-09-25 10:06:03 +0200

    install the mui icons

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
-import { Box, TextField } from "@mui/material";
 import React from "react";
+import { Box, InputAdornment, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
 
 export const Home = () => {
   return (
@@ -9,7 +10,16 @@ export const Home = () => {
         <Link to="/">Home</Link>
       </Box>
       <Box sx={{ display: "flex", justifyContent: "flex-end", gap: "10px" }}>
-        <TextField label="Search" variant="outlined" 
+        <TextField
+          label="Search"
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
         />
         <Link to="dashboard">DashBoard</Link>
         <Link to="dashboard">Login</Link>
