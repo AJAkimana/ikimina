85687851763171881b49aeff5b14d3fdc5fa8441
“Kevine <ikevine2016@gmail.com>
2024-10-04 11:42:45 +0200

    adding NavBar

--- src/app.routes.js
+++ src/app.routes.js
@@ -4,10 +4,12 @@ import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Box } from "@mui/material";
 import { DashBoard } from "./pages/dashboard";
+import { NavBar } from "./common/components/nav.bar";
 
 export const AppRoutes = () => {
   return (
     <Box>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,61 +1,14 @@
 import React from "react";
-import { Box, InputAdornment, TextField } from "@mui/material";
+import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
-import SearchIcon from "@mui/icons-material/Search";
-import BungalowIcon from "@mui/icons-material/Bungalow";
 
 export const Home = () => {
   return (
-    <Box
-      sx={{
-        margin: "30px",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          gap: "20px",
-        }}
-      >
-        <BungalowIcon />
-        <Link to="/" style={{ textDecoration: "none" }}>
-          Home
-        </Link>
-      </Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          gap: "50px",
-          alignItems: "center",
-        }}
-      >
-        <TextField
-          size="small"
-          label="Search"
-          variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Link to="dashboard" style={{ textDecoration: "none" }}>
-          DashBoard
-        </Link>
-        <Link to="dashboard" style={{ textDecoration: "none" }}>
-          Login
-        </Link>
-      </Box>
+    <Box>
+      <h1>This is the Home page</h1>
+      <Link to="/login" style={{ textDecoration: "none" }}>
+        Click to view our Login page
+      </Link>
     </Box>
   );
 };
